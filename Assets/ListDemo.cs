8849350fda19bb69d21f9528f7aa8c284193284f
Adrian Dorey <144232716+adorey91@users.noreply.github.com>
2023-11-20 10:56:24 -0400

    Initial commit

---
+++ Assets/ListDemo.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListDemo : MonoBehaviour
+{
+    [SerializeField]
+    private List<string> DemoList;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
